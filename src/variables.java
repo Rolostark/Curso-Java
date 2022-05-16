95d90b530b1122e9f674814b408f372b564d51fe
Rolostark <rggpromador@gmail.com>
2022-05-16 18:20:06 -0500

    6.-Curso Java. Estructuras principales III. Declaración variables Eclipse

---
+++ src/variables.java
@@ -0,0 +1,36 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		/*
+		byte edad;
+		edad=35;
+		
+		System.out.println(edad);
+		*/
+		/*
+		short edad;
+		edad=35;
+		
+		System.err.println(edad);
+		*/
+		/*
+		int edad;
+		edad=35;
+		
+		System.err.println(edad);
+		*/
+		
+		//comentario de una linea
+		//el codigo ejecuta de arriba ==> abajo
+		int edad =35; //inicia
+		edad=75;      //se canbia el valor de la variable
+		System.err.println(edad);
+		
+		/*comentarios
+		de varias lineas
+		en el codigo*/
+
+	}
+
+}
