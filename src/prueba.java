b70817a3bcc9ec4bd8cc4c4fbc2184194b9c1047
Rolostark <rggpromador@gmail.com>
2022-05-16 18:48:23 -0500

    13.-Curso Java. Acercamiento a la API  Paquetes

---
+++ src/prueba.java
@@ -0,0 +1,15 @@
+import java.util.Locale; //del paquete util importamos metodo Locale
+import java.util.Scanner; //del paquete util importamos metodo Scanner;
+//import java.util.*; //importamos todo el paquete util
+
+
+public class prueba {
+
+	public static void main(String[] args) {
+		String nombre;
+		Scanner objeto1;
+		Locale objeto2;
+
+	}
+
+}
