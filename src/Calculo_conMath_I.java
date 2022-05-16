0c08aae8fb48d3bde4d2dd8f738544a736716c39
Rolostark <rggpromador@gmail.com>
2022-05-16 18:36:16 -0500

    9.-Curso Java. Estructuras principales VI. Clase Math

---
+++ src/Calculo_conMath_I.java
@@ -0,0 +1,10 @@
+public class Calculo_conMath_I {
+
+	public static void main(String[] args) {
+		
+		double raiz = Math.sqrt(9); //  variable nombre_variable = clase.metodo(valor)
+		
+		System.out.println(raiz);
+		
+	}
+}
