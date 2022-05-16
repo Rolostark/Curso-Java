6075dfd6d765bc65243386e407ced7226ce7405b
Rolostark <rggpromador@gmail.com>
2022-05-16 18:29:45 -0500

    8.-Curso Java. Estructuras principales V. Constantes y Operadores II

---
+++ src/declaraciones_Operadores_II.java
@@ -0,0 +1,22 @@
+
+public class declaraciones_Operadores_II {
+
+	public static void main(String[] args) {
+		/*
+		final double apulgadas =2.54; //constante
+		double cm=6; //variables
+		double resultado=cm/apulgadas; //variable
+		
+		System.out.println("En "+cm+" cm hay "+resultado+" pulgadas" );
+		*/
+		
+		int operador1,operador2,resultado; //declarar muchas variables
+		operador1 = 8;
+		operador2 = 7;
+		resultado =operador1 + operador2;
+		
+		System.out.println(resultado);
+		
+	}
+
+}
