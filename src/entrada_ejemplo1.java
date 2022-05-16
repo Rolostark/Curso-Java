fd8b46bbb7c207df91f122ac40b0b0848f7cd63e
Rolostark <rggpromador@gmail.com>
2022-05-16 18:53:23 -0500

    14.-Curso Java. Entrada Salida datos I

---
+++ src/entrada_ejemplo1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class entrada_ejemplo1 {
+
+	public static void main(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		
+		System.out.println("introduce tu nombre, por favor");
+		
+		String nombre_usuario=entrada.nextLine();
+		
+		System.out.println("introduce tu edad, por favor");
+		
+		int edad=entrada.nextInt();
+		
+		System.out.println("Hola "+nombre_usuario+". El año que viene tendras "+(edad+1)+" años");
+
+	}
+
+}
