d71f6eac4e2ba5fe16a41a4bf77514aaabfebd29
Rolostark <rggpromador@gmail.com>
2022-05-16 18:26:50 -0500

    7.-Curso Java. Estructuras principales IV. Constantes y Operadores

---
+++ src/declaraciones_Operadores_I.java
@@ -0,0 +1,16 @@
+
+public class declaraciones_Operadores_I {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b;
+		b=7;
+		int c=b/a;
+		
+		//c-=6; // decremento se le resta 6
+		
+		System.out.println(c);
+
+	}
+
+}
