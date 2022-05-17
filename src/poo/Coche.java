0c41e9bc6039c91b116ffa1a9ea28f6f4417ba97
Rolostark <rggpromador@gmail.com>
2022-05-17 13:28:36 -0500

    28.-Curso Java. POO II

---
+++ src/poo/Coche.java
@@ -0,0 +1,23 @@
+package poo; //Paquetes
+
+public class Coche { //Clase
+
+		//atributos propiedades
+		int ruedas; //caracteristicas
+		int largo;
+		int ancho;
+		int motor;
+		int peso;
+		
+		//metodo constructor
+		public Coche(){
+			ruedas=4;
+			largo=2000;
+			ancho=300;
+			motor=1600;
+			peso=500;
+			
+		}
+}
+
+
---
+++ src/poo/Uso_Coche.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		//OBJETO
+		Coche Renault=new Coche(); //INSTANCIAR CLASE. EJEMPLARIZANDO UNA CLASE
+		
+		System.out.println("Este coche tiene "+Renault.ruedas+" ruedas");
+		
+
+	}
+
+}
